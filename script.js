cc6702469e8d21decc671cd62dc9226da2781dad
Chris Patuzzo <chris@patuzzo.co.uk>
2022-11-28 13:18:49 +0000

    Add a setting to highlight the paragraph that is currently being played

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const settingsFunctions = {};
+const timeUpdateFunctions = {};
 
 const main = async () => {
   const data = await fetch("data.json").then(r => r.json());
@@ -8,6 +9,9 @@ const main = async () => {
 
   checkboxes.forEach(c => c.checked && applySetting(c, data));
   checkboxes.forEach(c => c.onchange = () => applySetting(c, data));
+
+  const audioPlayer = document.getElementById("audio-player");
+  audioPlayer.ontimeupdate = () => applyTimeUpdate(audioPlayer);
 };
 
 const applySetting = (checkbox, data) => {
@@ -16,6 +20,10 @@ const applySetting = (checkbox, data) => {
   } else {
     settingsFunctions[`disable${checkbox.id}`](data);
   }
+};
+
+const applyTimeUpdate = (audioPlayer) => {
+  Object.values(timeUpdateFunctions).forEach(f => f(audioPlayer));
 };
 
 settingsFunctions.enableButtonsBetweenParagraphs = async (data) => {
@@ -44,6 +52,45 @@ settingsFunctions.enableButtonsBetweenParagraphs = async (data) => {
 settingsFunctions.disableButtonsBetweenParagraphs = () => {
   const playButtons = document.querySelectorAll(".button-between-paragraphs");
   playButtons.forEach(b => b.remove());
+};
+
+settingsFunctions.enableHighlightParagraph = (data) => {
+  const paragraphs = document.querySelectorAll("[data-beyondwords-paragraph-id]");
+
+  timeUpdateFunctions.highlightParagraph = (audioPlayer) => {
+    paragraphs.forEach(paragraph => {
+      paragraph.classList.remove("beyondwords-current");
+    });
+
+    let marker;
+    for (const [key, value] of Object.entries(data.timestamps)) {
+      if (value < audioPlayer.currentTime + 0.5) {
+        marker = key;
+      }  else {
+        break;
+      }
+    }
+
+    paragraphs.forEach(paragraph => {
+      const paragraphId = paragraph.getAttribute("data-beyondwords-paragraph-id");
+      if (paragraphId === marker) {
+        paragraph.classList.add("beyondwords-current");
+      }
+    });
+  }
+
+  const audioPlayer = document.getElementById("audio-player");
+  timeUpdateFunctions.highlightParagraph(audioPlayer);
+};
+
+settingsFunctions.disableHighlightParagraph = () => {
+  delete timeUpdateFunctions.highlightParagraph;
+
+  const paragraphs = document.querySelectorAll("[data-beyondwords-paragraph-id]");
+
+  paragraphs.forEach(paragraph => {
+    paragraph.classList.remove("beyondwords-current");
+  });
 };
 
 main();
