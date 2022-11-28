87afd7de9ebcfa915799de3736453e12761170a8
Chris Patuzzo <chris@patuzzo.co.uk>
2022-11-28 11:57:37 +0000

    Dynamically add buttons between paragraphs based on a prototype setting
    I want each feature of the prototype to be toggle-able so that we can
    play with the feel of the user interface to understand what works best.

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+const settingsFunctions = {};
+
 const main = async () => {
   const data = await fetch("data.json").then(r => r.json());
 
+  const prototypeSettings = document.getElementById("prototype-settings");
+  const checkboxes = prototypeSettings.querySelectorAll("input[type='checkbox']");
+
+  checkboxes.forEach(c => c.checked && applySetting(c, data));
+  checkboxes.forEach(c => c.onchange = () => applySetting(c, data));
+};
+
+const applySetting = (checkbox, data) => {
+  if (checkbox.checked) {
+    settingsFunctions[`enable${checkbox.id}`](data);
+  } else {
+    settingsFunctions[`disable${checkbox.id}`](data);
+  }
+};
+
+settingsFunctions.enableButtonsBetweenParagraphs = async (data) => {
   const audioPlayer = document.getElementById("audio-player");
   const paragraphs = document.querySelectorAll("[data-beyondwords-paragraph-id]");
 
@@ -9,6 +27,7 @@ const main = async () => {
     const timestamp = data.timestamps[paragraphId];
 
     const playButton = document.createElement("button");
+    playButton.classList.add("button-between-paragraphs");
     paragraph.parentNode.insertBefore(playButton, paragraph);
 
     const minutes = String(Math.floor(timestamp / 60)).padStart(2, "0");
@@ -20,6 +39,11 @@ const main = async () => {
       audioPlayer.play();
     }
   });
-}
+};
+
+settingsFunctions.disableButtonsBetweenParagraphs = () => {
+  const playButtons = document.querySelectorAll(".button-between-paragraphs");
+  playButtons.forEach(b => b.remove());
+};
 
 main();
