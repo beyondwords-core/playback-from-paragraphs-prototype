b338a0852d93eca58c6fad0bd2a7eec0297a2ede
Chris Patuzzo <chris@patuzzo.co.uk>
2023-07-28 11:53:16 +0100

    Regenerate all videos and write a script to make this easier

---
+++ generate_videos.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+# Fox News:
+
+pushd ../video_generator
+cat mocks/fox-news.json | cargo run --release -- -o landscape
+popd
+mv ../video_generator/landscape.mp4 fox-news/
+
+
+pushd ../video_generator
+cat mocks/fox-news.json | cargo run --release -- -o portrait -w 720 -h 1280
+popd
+mv ../video_generator/portrait.mp4 fox-news/
+
+
+pushd ../video_generator
+cat mocks/fox-news.json | cargo run --release -- -o square -w 720 -h 720
+popd
+mv ../video_generator/square.mp4 fox-news/
+
+
+# JP Morgan:
+
+pushd ../video_generator
+cat mocks/jp-morgan.json | cargo run --release -- -o landscape
+popd
+mv ../video_generator/landscape.mp4 jp-morgan/
+
+
+pushd ../video_generator
+cat mocks/jp-morgan.json | cargo run --release -- -o portrait -w 720 -h 1280
+popd
+mv ../video_generator/portrait.mp4 jp-morgan/
+
+
+pushd ../video_generator
+cat mocks/jp-morgan.json | cargo run --release -- -o square -w 720 -h 720
+popd
+mv ../video_generator/square.mp4 jp-morgan/
+
+
+# NY Post:
+
+pushd ../video_generator
+cat mocks/ny-post.json | cargo run --release -- -o landscape
+popd
+mv ../video_generator/landscape.mp4 ny-post/
+
+
+pushd ../video_generator
+cat mocks/ny-post.json | cargo run --release -- -o portrait -w 720 -h 1280
+popd
+mv ../video_generator/portrait.mp4 ny-post/
+
+
+pushd ../video_generator
+cat mocks/ny-post.json | cargo run --release -- -o square -w 720 -h 720
+popd
+mv ../video_generator/square.mp4 ny-post/
+
+
+# Reuters:
+
+pushd ../video_generator
+cat mocks/reuters.json | cargo run --release -- -o landscape
+popd
+mv ../video_generator/landscape.mp4 reuters/
+
+
+pushd ../video_generator
+cat mocks/reuters.json | cargo run --release -- -o portrait -w 720 -h 1280
+popd
+mv ../video_generator/portrait.mp4 reuters/
+
+
+pushd ../video_generator
+cat mocks/reuters.json | cargo run --release -- -o square -w 720 -h 720
+popd
+mv ../video_generator/square.mp4 reuters/
+
+
+# Sky News:
+
+pushd ../video_generator
+cat mocks/sky-news.json | cargo run --release -- -o landscape
+popd
+mv ../video_generator/landscape.mp4 sky-news/
+
+
+pushd ../video_generator
+cat mocks/sky-news.json | cargo run --release -- -o portrait -w 720 -h 1280
+popd
+mv ../video_generator/portrait.mp4 sky-news/
+
+
+pushd ../video_generator
+cat mocks/sky-news.json | cargo run --release -- -o square -w 720 -h 720
+popd
+mv ../video_generator/square.mp4 sky-news/
+
+
+# The Sun:
+
+pushd ../video_generator
+cat mocks/the-sun.json | cargo run --release -- -o landscape
+popd
+mv ../video_generator/landscape.mp4 the-sun/
+
+
+pushd ../video_generator
+cat mocks/the-sun.json | cargo run --release -- -o portrait -w 720 -h 1280
+popd
+mv ../video_generator/portrait.mp4 the-sun/
+
+
+pushd ../video_generator
+cat mocks/the-sun.json | cargo run --release -- -o square -w 720 -h 720
+popd
+mv ../video_generator/square.mp4 the-sun/
